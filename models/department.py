eef14d72d464aedef68c3cdbb0a01a34f1af5a4d
Laura <launaizq@gmail.com>
2025-04-08 16:28:12 +0000

    feat: add department model with its fields

---
+++ models/department.py
@@ -0,0 +1,14 @@
+# -*- coding: utf -8 -*-
+from odoo import models, fields, api
+
+class Department(models.Model):
+    
+    _name = 'stage_sec.department'
+    _description = 'Department'
+    
+	department_name = fields.Char(string = "Department name", required=True)
+    department_location = fields.Char(string = "Location", required=True)
+    department_description = fields.Char(string ="Department description", required=True)
+    number_employees = fields.Char(string = "Number of employees", required=True)
+    department_phone = fields.Integer(string = "Phone of department", required=True)
+    department_email = fields.Char(string = "Department email", required=True)
