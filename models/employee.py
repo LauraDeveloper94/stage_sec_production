c21cb948846dbb348d0f8a663df3143cb7968e46
Laura <launaizq@gmail.com>
2025-04-08 17:20:54 +0000

    fix: the fields couldn't upload before because I didn't save the information

---
+++ models/employee.py
@@ -0,0 +1,17 @@
+class Employee(models.Model):
+    
+    _name = 'stage_sec.employee'
+    _description = 'Employee'
+    
+    id_number = fields.Char(string="ID number", required=True)
+	employee_name = fields.Char(string="Employee name", required=True)
+	email = fields.Char(string="Email", required=True)
+    surname1 = fields.Char(string = "First surname", required=True)
+    surname2 = fields.Char(string = "Second surname")
+    phone_number = fields.Integer(string = "Phone number")
+    employee_type = fields.Selection(
+        [('factory_worker', 'Factory_worker'), ('engineer', 'Engineer')],
+        string="Type of employee", required=True
+    )
+    employee_address = fields.Char(string = "Employee address", required=True)
+    position = fields.Char(string = "Employee position", required=True)
