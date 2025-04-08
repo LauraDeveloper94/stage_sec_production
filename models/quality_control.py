8346956a610a7b463249c0e50e782e6aa08c9a44
Laura <launaizq@gmail.com>
2025-04-08 17:28:14 +0000

    fix: the fields couldn't upload before because I didn't save the information

---
+++ models/quality_control.py
@@ -0,0 +1,14 @@
+# -*- coding: utf -8 -*-
+from odoo import models, fields, api
+
+class Quality_control(models.Model):
+        
+    _name = 'stage_sec.quality_control'
+    _description = 'Quality control'
+        
+    control_date = fields.Date(string = "Control date", required=True)
+    quality_status = fields.Char(string = "Quality status", required=True)   
+    qc_report = fields.Char(string = "Report of the quality status")
+    retest_required = fields.Boolean(string = "Retest required", required=True)
+    approved_quantity = fields.Integer(string = "Approved quantity", required=True)
+    rejected_quantity = fields.Integer(string = "Rejected quantity", required=True)
