57ecc562239613ab72c43f95a713900fe5f31892
Laura <launaizq@gmail.com>
2025-04-08 17:26:32 +0000

    fix: the fields couldn't upload before because I didn't save the information

---
+++ models/product.py
@@ -0,0 +1,15 @@
+# -*- coding: utf -8 -*-
+from odoo import models, fields, api
+
+class Product(models.Model):
+    
+    _name = 'stage_sec.product'
+    _description = 'Product'
+    
+	product_name = fields.Char(string="Product name", required=True)
+    product_description = fields.Char(string="Product description", required=True)
+    price = fields.Float(string = "Price of the product", required=True)
+    dimensions = fields.Float(string = "Dimensions of the product", required=True)
+    weight = fields.Float(string = "Weight of the product", required=True)
+    stock_min = fields.Integer(string = "Minimum stock", required=True)
+    stock_max = fields.Integer(string = "Maximum stock", required=True)
