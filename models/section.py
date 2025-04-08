257e8e756c30fb6d37b24444d3bdb8cd9ad15f1a
Laura <launaizq@gmail.com>
2025-04-08 17:29:09 +0000

    fix: the fields couldn't upload before because I didn't save the information

---
+++ models/section.py
@@ -0,0 +1,10 @@
+# -*- coding: utf -8 -*-
+from odoo import models, fields, api
+
+class Section(models.Model):
+
+    _name = 'stage_sec.section'
+    _description = 'Section'
+
+    section_name = fields.Char(string = "Section name", required=True)
+    section_location = fields.Char(string = "Section location", required=True)
