9154f72a01c45071e74e857fff8889d328c541ea
Laura <launaizq@gmail.com>
2025-04-08 17:24:18 +0000

    fix: the fields couldn't upload before because I didn't save the information

---
+++ models/material.py
@@ -0,0 +1,16 @@
+# -*- coding: utf -8 -*-
+from odoo import models, fields, api
+
+class Material(models.Model):
+    
+    _name = 'stage_sec.material'
+    _description = 'Material'
+    
+	material_name = fields.Char(string="Material", required=True)
+    material_type = fields.Char(string="Type of material", required=True)
+    price = fields.Float(string = "Price of the material", required=True)
+    dimensions = fields.Float(string = "Dimensions of the material", required=True)
+    weight = fields.Float(string = "Weight of the material", required=True)
+    stock_min = fields.Integer(string = "Minimum stock", required=True)
+    stock_max = fields.Integer(string = "Maximum stock", required=True)
+    availability = fields.Integer(string = "Availability of the material", required=True)
