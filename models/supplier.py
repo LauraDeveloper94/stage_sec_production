d2244a6b05818dc5d935dbe0eebbd2849c5d762e
Laura <launaizq@gmail.com>
2025-04-08 17:29:41 +0000

    fix: the fields couldn't upload before because I didn't save the information

---
+++ models/supplier.py
@@ -0,0 +1,18 @@
+# -*- coding: utf -8 -*-
+from odoo import models, fields, api
+
+class Supplier(models.Model):
+    
+    _name = 'stage_sec.supplier'
+    _description = 'Supplier'
+    
+	supplier_name = fields.Char(string="Supplier name", required=True)
+	email = fields.Char(string="Email", required=True)
+    surname1 = fields.Char(string ="First surname", required=True)
+    surname2 = fields.Char(string = "Second surname")
+    phone_number = fields.Char(string = "Phone number")
+    supplier_type = fields.Selection(
+        [('raw_material', 'Raw_material'), ('finished_goods', 'Finished_goods')],
+        string="Type of supplier", required=True
+    )
+    supplier_address = fields.Char(string = "Supplier address", required=True)
