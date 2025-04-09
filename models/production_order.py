c816d4cd8fc002c7b05f1370c7b81463a3eaa414
Laura <launaizq@gmail.com>
2025-04-09 18:53:21 +0000

    fix: identation Class production_order

--- models/production_order.py
+++ models/production_order.py
@@ -1,12 +1,11 @@
-# -*- coding: utf -8 -*-
+# -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
 class Production_order(models.Model):
+    _name = 'stage_sec.production_order'
+    _description = 'Production order'
     
-	_name = 'stage_sec.production_order'
-	_description = 'Production order'
-    
-    start_date = fields.Date(string = "Start date", required=True)
-    order_status = fields.Char(string = "Order status", required=True)
-    end_date = fields.Date(string = "End date", required=True)
-    priority_level = fields.Char(string = "Priority level", required=True)
+    start_date = fields.Date(string="Start date", required=True)
+    order_status = fields.Char(string="Order status", required=True)
+    end_date = fields.Date(string="End date", required=True)
+    priority_level = fields.Char(string="Priority level", required=True)
