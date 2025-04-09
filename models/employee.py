c8ff6abc9daa6c7a2be756f11112b44415190d25
Laura <launaizq@gmail.com>
2025-04-09 19:08:43 +0000

    fix: identation Class employee, added coding utf 8 and changed email field from Integer to Char

--- models/employee.py
+++ models/employee.py
@@ -1,14 +1,16 @@
+# -*- coding: utf -8 -*-
+
 class Employee(models.Model):
     
     _name = 'stage_sec.employee'
     _description = 'Employee'
     
     id_number = fields.Char(string="ID number", required=True)
-	employee_name = fields.Char(string="Employee name", required=True)
-	email = fields.Char(string="Email", required=True)
+    employee_name = fields.Char(string="Employee name", required=True)
+    email = fields.Char(string="Email", required=True)
     surname1 = fields.Char(string = "First surname", required=True)
     surname2 = fields.Char(string = "Second surname")
-    phone_number = fields.Integer(string = "Phone number")
+    phone_number = fields.Char(string = "Phone number")
     employee_type = fields.Selection(
         [('factory_worker', 'Factory_worker'), ('engineer', 'Engineer')],
         string="Type of employee", required=True
