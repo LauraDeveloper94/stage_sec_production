65f71b3254f74e4f0f5731db6da6a17daf6dcf62
Laura <launaizq@gmail.com>
2025-04-15 19:32:23 +0000

    "fix: variable names

--- models/quality_control.py
+++ models/quality_control.py
@@ -7,7 +7,7 @@ class Quality_control(models.Model):
     _description = 'Quality control'
         
     control_date = fields.Date(string = "Control date", required=True)
-    quality_status = fields.Char(string = "Quality status", required=True)   
+    status = fields.Char(string = "Quality status", required=True)   
     qc_report = fields.Char(string = "Report of the quality status")
     retest_required = fields.Boolean(string = "Retest required", required=True)
     approved_quantity = fields.Integer(string = "Approved quantity", required=True)
