8ebc851a02c8ccce7398743b7abc1b01299fffbf
Laura <launaizq@gmail.com>
2025-04-15 20:37:22 +0000

    "fix: variable name"

--- models/section.py
+++ models/section.py
@@ -6,5 +6,5 @@ class Section(models.Model):
     _name = 'stage_sec_production.section'
     _description = 'Section'
 
-    name = fields.Char(string = "Section name", required=True)
-    location = fields.Char(string = "Section location", required=True)
+    name = fields.Char(string = "Name", required=True)
+    location = fields.Char(string = "Location", required=True)
