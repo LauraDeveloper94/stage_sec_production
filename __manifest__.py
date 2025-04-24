e8b3a456be280912c9fd2ceaec754fbcc901de28
Laura <launaizq@gmail.com>
2025-04-24 17:35:33 +0000

    add initial clients dataset

--- __manifest__.py
+++ __manifest__.py
@@ -25,7 +25,9 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'data/client.xml',
     ],
+
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
